6dec81f5389f585b88ad3d2af0affa2ce3772477
Tiago <thiagowendtpercyak@outlook.com>
2023-06-22 17:43:59 -0300

    first commit

---
+++ quiz/src/App.jsx
@@ -0,0 +1,15 @@
+//react, componentes, estáticos
+
+import "./App.css";
+import Welcome from "./components/Welcome";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Quiz App</h1>
+      <Welcome />
+    </div>
+  );
+}
+
+export default App;
